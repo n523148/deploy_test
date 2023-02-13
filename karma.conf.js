774fa524b9eab85048836b5eb74b6d6ed62c5e4e
MOSCOW\Valeriia_Iavorskaia <valeriia_iavorskaia@epam.com>
2023-02-13 17:12:09 +0100

    moved everything one directory up

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'deploy_test',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
